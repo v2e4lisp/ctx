88afef99e1e244908ef5543d6ce6e7c33664ca1f
wenjun.yan <mylastnameisyan@gmail.com>
2015-06-02 00:16:14 +0900

    fix bug.

--- ctx.go
+++ ctx.go
@@ -27,23 +27,14 @@ func (it *context) Set(key string, val interface{}) {
 	it.d[key] = val
 }
 
-func For(r *http.Request) context {
-	c, _ := contexts[r]
-	return c
-}
-
-func create(r *http.Request) {
-	contexts[r] = context{d: make(map[string]interface{})}
-}
-
-func remove(r *http.Request) {
-	delete(contexts, r)
-}
+func For(r *http.Request) *context                               { c, _ := contexts[r]; return &c }
+func Set(r *http.Request, key string, val interface{})           { For(r).Set(key, val) }
+func Get(r *http.Request, key string) (val interface{}, ok bool) { val, ok = For(r).Get(key); return }
 
 var Handler = func(h http.Handler) http.Handler {
 	f := func(w http.ResponseWriter, r *http.Request) {
-		create(r)
-		defer remove(r)
+		contexts[r] = context{d: make(map[string]interface{})}
+		defer delete(contexts, r)
 		h.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(f)
